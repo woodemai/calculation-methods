84ba703ddc303391aab854761bf15b08e3bb524a
woodemai <n.savchenkoo73@gmail.com>
2024-03-27 02:32:27 +0300

    add trapezoid method

--- numerical-integration/main.py
+++ numerical-integration/main.py
@@ -1,11 +1,17 @@
 import timeit
 from functools import partial
-
+from methods import rectangle_method, trapezoid_method
 from sympy import symbols, diff, lambdify
 
 x = symbols('x')
-y = x / (x ** 5 + 1)
+# your function
+# y = x / (x ** 5 + 1)
+y = 16 * x / (x ** 8 + 1)
 
+def f(x):
+    # your function
+    # return x / (x ** 5 + 1)
+    return  16 * x / (x ** 8 + 1)
 
 def derivative_function(x, y):
     derivative = diff(y, x)
@@ -15,25 +21,11 @@ def derivative_function(x, y):
     return df, ddf
 
 
-def rectangle_method(f, a, b, n):
-    h = (b - a) / n
-    return h * sum(f(a + i * h) for i in range(n))
-
-
 def measure_execution_time(func, *args, **kwargs):
     result = func(*args, **kwargs)
     partial_func = partial(func, *args, **kwargs)
     execution_time = timeit.timeit(partial_func, number=10)
     return execution_time, result
-
-
-def f(x):
-    return x / (x ** 5 + 1)
-
-
-a = 0
-b = 5
-n = 100
 
 
 def rectangular_rule_error(a, b, h, f_second_derivative):
@@ -42,9 +34,16 @@ def rectangular_rule_error(a, b, h, f_second_derivative):
     return error
 
 
-execution_time, result = measure_execution_time(rectangle_method, f, a, b, n)
-df, ddf = derivative_function(x, y)
-error = rectangular_rule_error(a, b, (b - a) / n, ddf)
-print(f"The integral of f from {a} to {b} is approximately {result}.")
-print(f"The execution time was {execution_time} seconds.")
-print(f"Error: {error}")
+a = 0
+b = 1
+n = 1000000
+methods = [rectangle_method, trapezoid_method]
+
+for method in methods:
+    execution_time, result = measure_execution_time(method, f, a, b, n)
+    df, ddf = derivative_function(x, y)
+    error = rectangular_rule_error(a, b, (b - a) / n, ddf)
+    print(f"The integral of f from {a} to {b} is approximately {result}.")
+    print(f"The execution time was {execution_time} seconds.")
+    print(f"Error: {error}")
+    print('\n')
