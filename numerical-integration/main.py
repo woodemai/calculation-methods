af7b5bcae6f152722c0d114bdbbab4cbdf6f70de
woodemai <n.savchenkoo73@gmail.com>
2024-04-03 09:26:38 +0300

    add simpson method

--- numerical-integration/main.py
+++ numerical-integration/main.py
@@ -1,49 +1,27 @@
 import timeit
 from functools import partial
-from methods import rectangle_method, trapezoid_method
-from sympy import symbols, diff, lambdify
+from methods import rectangle_method, trapezoid_method, simpson_method
 
-x = symbols('x')
-# your function
-# y = x / (x ** 5 + 1)
-y = 16 * x / (x ** 8 + 1)
 
 def f(x):
-    # your function
-    # return x / (x ** 5 + 1)
-    return  16 * x / (x ** 8 + 1)
-
-def derivative_function(x, y):
-    derivative = diff(y, x)
-    derivative2 = diff(derivative, x)
-    df = lambdify(x, derivative, 'numpy')
-    ddf = lambdify(x, derivative2, 'numpy')
-    return df, ddf
+    return 2 / (pow(x, 2 / 3) + pow(x, 1 / 3)) ** 2
 
 
 def measure_execution_time(func, *args, **kwargs):
-    result = func(*args, **kwargs)
+    res, err = func(*args, **kwargs)
     partial_func = partial(func, *args, **kwargs)
     execution_time = timeit.timeit(partial_func, number=10)
-    return execution_time, result
+    return execution_time, res, err
 
 
-def rectangular_rule_error(a, b, h, f_second_derivative):
-    max_derivative = max(f_second_derivative(x) for x in range(a, b + 1))
-    error = (b - a) * h ** 2 / 24 * max_derivative
-    return error
-
-
-a = 0
-b = 1
-n = 1000000
-methods = [rectangle_method, trapezoid_method]
+a = 1
+b = 8
+n = 1000
+methods = [rectangle_method, trapezoid_method, simpson_method]
 
 for method in methods:
-    execution_time, result = measure_execution_time(method, f, a, b, n)
-    df, ddf = derivative_function(x, y)
-    error = rectangular_rule_error(a, b, (b - a) / n, ddf)
-    print(f"The integral of f from {a} to {b} is approximately {result}.")
+    execution_time, res, err = measure_execution_time(method, f, a, b, n)
+    print(f"The integral of f from {a} to {b} is approximately {res}.")
     print(f"The execution time was {execution_time} seconds.")
-    print(f"Error: {error}")
+    print(f"Error: {err}")
     print('\n')
--- numerical-integration/methods.py
+++ numerical-integration/methods.py
@@ -1,8 +1,62 @@
+from sympy import symbols, diff, lambdify
+
+x = symbols('x')
+y = 2 / (pow(x, 2 / 3) + pow(x, 1 / 3)) ** 2
+
+
+def derivative_function(x, y):
+    derivative = diff(y, x)
+    derivative2 = diff(derivative, x)
+    derivative3 = diff(derivative2, x)
+    derivative4 = diff(derivative3, x)
+
+    df1 = lambdify(x, derivative, 'numpy')
+    df2 = lambdify(x, derivative2, 'numpy')
+    df3 = lambdify(x, derivative3, 'numpy')
+    df4 = lambdify(x, derivative4, 'numpy')
+
+    return df1, df2, df3, df4
+
+
+df1, df2, df3, df4 = derivative_function(x, y)
+
+
+def rectangular_method_error(a, b, h, f_second_derivative):
+    max_derivative = max(f_second_derivative(x) for x in range(a, b + 1))
+    error = (b - a) * h ** 2 / 24 * max_derivative
+    return error
+
+
+def trapezoid_method_error(a, b, h, f_second_derivative):
+    max_derivative = max(f_second_derivative(x) for x in range(a, b + 1))
+    error = (b - a) * h ** 2 / 12 * max_derivative
+    return error
+
+
+def simpson_method_error(a, b, h, df4):
+    max_df = max(df4(x) for x in range(a, b + 1))
+    error = (b - a) * pow(h, 4) / 12 * max_df
+    return error
+
+
 def rectangle_method(f, a, b, n):
     h = (b - a) / n
-    return h * sum(f(a + i * h) for i in range(n))
+    res = h * sum(f(a + i * h) for i in range(n))
+    err = trapezoid_method_error(a, b, h, df2)
+    return res, err
 
 
 def trapezoid_method(f, a, b, n):
     h = (b - a) / n
-    return h * (0.5 * f(a) + 0.5 * f(b) + sum(f(a + i * h) for i in range(1, n)))
+    res = h * (0.5 * f(a) + 0.5 * f(b) + sum(f(a + i * h) for i in range(1, n)))
+    err = rectangular_method_error(a, b, h, df2)
+    return res, err
+
+
+def simpson_method(f, a, b, n):
+    h = (b - a) / n
+    res = h / 3 * (f(a) + f(b) + 4 * sum(f(a + i * h) for i in range(1, n, 2)) + 2 * sum(
+        f(a + i * h) for i in range(2, n, 2)))
+
+    err = simpson_method_error(a, b, h, df4)
+    return res, err
